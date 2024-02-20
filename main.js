50d0ea70f64ec1e43a920699844dc413de2cc7c3
Krzysztof Woszek <115538170+kwoszek@users.noreply.github.com>
2024-02-20 12:04:06 +0100

    i will kill myself today at exactly 12:38

---
+++ main.js
@@ -0,0 +1,6 @@
+console.log("jebac czqrnych")
+const canvas = document.getElementById("buni");
+const ctx = canvas.getContext("2d");
+const image = document.getElementById("buniimg");
+
+  ctx.drawImage(image, 0, 0, 100, 100);
